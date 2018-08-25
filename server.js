5249b7a5533b2dd7649727891a80d1f768960a4d
Abhi <ranzo9430@gmail.com>
2018-08-25 18:35:15 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,76 @@
+const express = require('express');
+var app  = express();
+//dynamic rendering emplate using hbs
+const hbs = require('hbs');
+const fs = require('fs');
+
+//using middleware
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+
+
+
+//Registering middleware
+//inside this middleware doesnt call next
+app.use((req,res, next)=>{
+   
+   //now logout all the request that comes to the server
+   //when someone made a request to specific url
+   let now = new Date().toString();
+   var log = `${now}: ${req.method} ${req.url}`;
+
+   console.log(log);
+   fs.appendFile('server.log', log + '\n', (err)=>{
+       if(err){
+           console.log("Unable to append file: ");
+       }
+   })
+
+
+
+
+   next();
+
+
+});
+app.use((req, res, next)=>{
+    res.render('maintainance.hbs');
+})
+app.use(express.static(__dirname + '/public'));
+//Register an handler for a http get request
+hbs.registerHelper('getCurrentYear', ()=>{
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text)=>{
+    return text.toUpperCase();
+})
+
+app.get('/',(request, response)=>{
+    //passing template name and data
+  response.render('home.hbs',{
+      pageTitle: 'Home page',
+      currentYear: new Date().getFullYear(),
+      welcomeMessage: 'Welcome to ranzo website'
+  })
+
+    
+});
+
+app.get('/about',(req, res)=>{
+     res.render('about.hbs', {
+         pageTitle: 'About my page',
+         currentYear: new Date().getFullYear()
+     });
+});
+
+app.get('/bad', (req, res)=>{
+    res.send({
+        errorMessage: 'Unable to fetch data'
+    });
+});
+//Bind the application to port in our machine
+
+app.listen(3000, ()=>{
+    console.log('Server is up on the port 3000');
+});
